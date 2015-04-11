0ad684d7930c858c738b31d3d55e3e7c54e632d2
Rob Nugen <rob@robnugen.com>
2015-04-12 00:28:58 +0900

    Drag and drop sprites, but slider is ignored

---
+++ Assets/scripts/Draggable.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Draggable : MonoBehaviour {
+	Vector2 startPos;
+	SliderJoint2D myVerticalSlider;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+
+	void addSliderJoint() {
+		if (myVerticalSlider == null) {
+			myVerticalSlider = gameObject.AddComponent<SliderJoint2D> () as SliderJoint2D;
+			myVerticalSlider.angle = 90;
+		}
+	}
+
+
+	void OnMouseUp() {
+		addSliderJoint ();
+		GetComponent<Rigidbody2D> ().gravityScale = 1;
+	}
+
+	void OnMouseDrag() {        
+		// Convert mouse position to world position
+		Vector2 p = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		
+		// Keep it in a certain radius
+		Vector2 dir = p - startPos;
+		
+		// Set the Position
+		transform.position = startPos + dir;
+	}
+	
+}
---
+++ Assets/scripts/GameGrid.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameGrid : MonoBehaviour {
+
+	public float horizSpaceBetweenStartSquares = 1.5f;
+	public float topY = 5f;
+	public float bottomY = -5f;
+	public float gridCenter = 0;
+
+	public static GameGrid instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
+
+	// Available Gems
+	public GameObject[] gems;
+
+	void Start () {
+		spawnAt (gridCenter - horizSpaceBetweenStartSquares, topY);
+		spawnAt (gridCenter, topY);
+		spawnAt (gridCenter + horizSpaceBetweenStartSquares, topY);
+	}
+	
+	void spawnAt(float x, float y) {
+		int index = Random.Range(0, gems.Length);
+		Instantiate(gems[index], new Vector2(x, y), Quaternion.identity);
+	}
+
+}
